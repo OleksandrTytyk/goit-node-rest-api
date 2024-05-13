99396dc0fc8288b8d0653174af0deabbf62be15b
Oleksandr Tytyk <aleks.tit3@gmail.com>
2024-05-13 14:03:18 +0300

    first release

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -12,28 +12,34 @@ import {
   updateContactById,
 } from "../services/contactsServices.js";
 
-export const getAllContacts = async (req, res) => {
+export const getAllContacts = async (_, res) => {
   const contacts = await listContacts();
+
   res.status(200).json(contacts);
 };
+
 export const getOneContact = async (req, res, next) => {
   try {
     const { id } = req.params;
+
     const contact = await getContactById(id);
+
     if (!contact) {
       throw new HttpError(404, "Contact not found");
     } else {
       res.status(200).json(contact);
     }
   } catch (error) {
-    next(error); // Передаем ошибку следующему middleware
+    next(error);
   }
 };
 
 export const deleteContact = async (req, res, next) => {
   try {
     const { id } = req.params;
+
     const contact = await removeContact(id);
+
     if (!contact) {
       throw new HttpError(404, "Contact not found");
     } else {
@@ -57,6 +63,7 @@ export const createContact = async (req, res, next) => {
     const formattedPhone = formatPhoneNumber(phone);
 
     const newContact = await addContact(name, email, formattedPhone);
+
     res.status(201).json(newContact);
   } catch (error) {
     next(error);
@@ -66,21 +73,21 @@ export const createContact = async (req, res, next) => {
 export const updateContact = async (req, res, next) => {
   try {
     const { id } = req.params;
-    // const { name, email, phone } = req.body;
 
     const { error } = updateContactSchema.validate(req.body);
     if (error) {
       throw new HttpError(400, error.message);
     }
 
-    // Обновляем контакт, передавая все три поля
+    if (Object.keys(req.body).length === 0) {
+      throw new HttpError(400, "Body must have at least one field");
+    }
+
     const updatedContact = await updateContactById(id, req.body);
 
-    // Проверяем, был ли найден контакт для обновления
     if (!updatedContact) {
       throw new HttpError(404, "Contact not found");
     }
-
     res.json(updatedContact);
   } catch (error) {
     next(error);
