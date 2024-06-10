6ce313ffa5895a61e7bb67807580181f3c325132
Oleksandr Tytyk <aleks.tit3@gmail.com>
2024-06-10 19:24:44 +0300

    First release

---
+++ schemas/userSchema.js
@@ -0,0 +1,17 @@
+import Joi from "joi";
+
+export const registerSchema = Joi.object({
+  password: Joi.string().required(),
+  email: Joi.string().required(),
+  subscription: Joi.string(),
+  token: Joi.string(),
+});
+
+export const loginSchema = Joi.object({
+  password: Joi.string().required(),
+  email: Joi.string().required(),
+});
+
+export const repeatVerifySchema = Joi.object({
+  email: Joi.string().required(),
+});
